a042f52e3ccabc0a6ed1a8a9a2a8b3b9eb63ef0d
Douglas Muth <douglas_muth@cable.comcast.com>
2018-04-06 15:03:44 -0400

    Snowdrift: version 1.0.1

--- Formula/snowdrift.rb
+++ Formula/snowdrift.rb
@@ -1,8 +1,8 @@
 class Snowdrift < Formula
   desc "App to perform testing and validation of firewall rules"
   homepage "https://github.com/Comcast/snowdrift"
-  url "https://github.com/Comcast/snowdrift/archive/snowdrift-1.0.tar.gz"
-  sha256 "f48b6beec8f6f93d1e57709f6ff8853352ccea7e21e28b53f9a31dacd3e27e96"
+  url "https://github.com/Comcast/snowdrift/archive/snowdrift-1.0.1.tar.gz"
+  sha256 "3d8a01b09874a95e475cbf0b8ab83142d1a8f8162da15c3a22df252d965b5ac5"
 
   def install
     bin.install "snowdrift"
