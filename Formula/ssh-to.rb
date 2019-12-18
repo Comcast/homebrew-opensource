e70c597e46c4db0c47d3ee0812db8ca7f8c82702
Douglas Muth <douglas_muth@cable.comcast.com>
2019-12-18 16:48:14 -0500

    Updated with version ssh-to 1.0.1.

--- Formula/ssh-to.rb
+++ Formula/ssh-to.rb
@@ -1,8 +1,8 @@
 class SshTo < Formula
   desc "Easily manage dozens or hundreds of machines via SSH"
   homepage "https://github.com/Comcast/ssh-to"
-  url "https://github.com/Comcast/ssh-to/archive/ssh-to-1.0.tar.gz"
-  sha256 "bcb8f4615c9f198c133678ea0245c9fa307f016130195815a4e534d2d8bea6b5"
+  url "https://github.com/Comcast/ssh-to/archive/ssh-to-1.0.1.tar.gz"
+  sha256 "40c5f7e9b7ce4f137245cdd48ba61e373e7400b5a7f9f5938ab542eef4e72968"
 
   def install
     bin.install "ssh-to"
